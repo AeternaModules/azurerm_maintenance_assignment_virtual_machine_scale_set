c2d32631755224d033cce3dcebb01f0a653bdd0e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:03:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "maintenance_assignment_virtual_machine_scale_sets_id" {
+  description = "Map of id values across all maintenance_assignment_virtual_machine_scale_sets, keyed the same as var.maintenance_assignment_virtual_machine_scale_sets"
+  value       = { for k, v in azurerm_maintenance_assignment_virtual_machine_scale_set.maintenance_assignment_virtual_machine_scale_sets : k => v.id }
+}
 output "maintenance_assignment_virtual_machine_scale_sets_location" {
   description = "Map of location values across all maintenance_assignment_virtual_machine_scale_sets, keyed the same as var.maintenance_assignment_virtual_machine_scale_sets"
   value       = { for k, v in azurerm_maintenance_assignment_virtual_machine_scale_set.maintenance_assignment_virtual_machine_scale_sets : k => v.location }
